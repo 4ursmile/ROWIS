2b904ab300bc69fc3f15c5a2391216e951559563
4ursmile <21521109@gm.uit.edu.vn>
2024-03-21 18:26:58 +0700

    add tile, database, audio system

--- sparseinst/sparseinst.py
+++ sparseinst/sparseinst.py
@@ -145,7 +145,7 @@ class SparseInst(nn.Module):
 
         #pred_scores = torch.sqrt(pred_scores * pred_objectness)
         obj_prob = torch.exp(-self.temperature * pred_objectness).unsqueeze(-1)
-        pred_scores = torch.sqrt(obj_prob * torch.sigmoid(pred_scores))
+        pred_scores = obj_prob * torch.sigmoid(pred_scores)
         for _, (scores_per_image, mask_pred_per_image, batched_input, img_shape) in enumerate(zip(
                 pred_scores, pred_masks, batched_inputs, image_sizes)):
 
