682b375340fac4b56494488234c102d451d3c311
4ursmile <21521109@gm.uit.edu.vn>
2024-03-28 21:53:58 +0700

    for colab

--- tools/train_net.py
+++ tools/train_net.py
@@ -239,8 +239,8 @@ def parameter_count_table(model: nn.Module, max_depth: int = 3) -> str:
 
 def main(args):
     os.system("nvidia-smi")
-    register_coco_instances("coco_train", {}, "/kaggle/input/coco-2017-dataset/coco2017/annotations/instances_train2017.json", "/kaggle/input/coco-2017-dataset/coco2017/train2017")
-    register_coco_instances("coco_val", {}, "/kaggle/input/coco-2017-dataset/coco2017/annotations/instances_val2017.json", "/kaggle/input/coco-2017-dataset/coco2017/val2017")
+    # register_coco_instances("coco_train", {}, "/kaggle/input/coco-2017-dataset/coco2017/annotations/instances_train2017.json", "/kaggle/input/coco-2017-dataset/coco2017/train2017")
+    # register_coco_instances("coco_val", {}, "/kaggle/input/coco-2017-dataset/coco2017/annotations/instances_val2017.json", "/kaggle/input/coco-2017-dataset/coco2017/val2017")
     cfg = setup(args)
     if args.eval_only:
         model = Trainer.build_model(cfg)
