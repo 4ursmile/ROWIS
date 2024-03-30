79b4d0898f3e4612de8f6416a96afc2fdefb7897
4ursmile <21521109@gm.uit.edu.vn>
2024-03-30 18:26:23 +0700

    add init

--- sparseinst/sparseinst.py
+++ sparseinst/sparseinst.py
@@ -137,7 +137,7 @@ class SparseInst(nn.Module):
         self.temperature = cfg.MODEL.OWIS.TEMPERATURE
         self.pred_per_image = cfg.MODEL.OWIS.PRED_PER_IMAGE
         self.temperature = cfg.MODEL.OWIS.TEMPERATURE/cfg.MODEL.OWIS.HIDDEN_DIM
-        print(f"Number of parameters: {parameter_count_table(self, 5)}")
+        print(f"Number of parameters: {parameter_count_table(self, 3)}")
         torch.autograd.set_detect_anomaly(True)
     def normalizer(self, image):
         image = (image - self.pixel_mean) / self.pixel_std
